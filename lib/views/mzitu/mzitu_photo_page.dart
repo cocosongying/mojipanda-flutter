51e1c657b06cff1d59195291c02e597135e2fed5
cocosongying <cocosongying@gmail.com>
2020-08-28 23:13:18 +0800

    refactor:相册布局

--- lib/views/mzitu/mzitu_photo_page.dart
+++ lib/views/mzitu/mzitu_photo_page.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
 class MzituPhotoPage extends StatefulWidget {
-  List images = [];
-  int index = 0;
-  PageController controller;
+  final int id;
+  MzituPhotoPage({this.id});
+
   @override
   _MzituPhotoPageState createState() => _MzituPhotoPageState();
 }
 
 class _MzituPhotoPageState extends State<MzituPhotoPage> {
   int currentIndex = 0;
+  List images = [];
+  PageController controller;
   @override
   void initState() {
-    currentIndex = widget.index;
-    widget.images = [
-      'https://imgpc.iimzt.com/2019/09/28b01.jpg',
-      'https://imgpc.iimzt.com/2019/09/28b56.jpg',
-      'https://imgpc.iimzt.com/2019/09/28b02.jpg'
+    images = [
+      'https://mojipanda.com/public/download/mzitu/28b01.jpg',
+      'https://mojipanda.com/public/download/mzitu/28b56.jpg',
+      'https://mojipanda.com/public/download/mzitu/28b02.jpg'
     ];
     super.initState();
   }
@@ -35,19 +36,40 @@ class _MzituPhotoPageState extends State<MzituPhotoPage> {
               child: Container(
                 child: PhotoViewGallery.builder(
                     scrollPhysics: const BouncingScrollPhysics(),
-                    backgroundDecoration: null,
-                    pageController: widget.controller,
-                    enableRotation: true,
+                    backgroundDecoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    pageController: controller,
+                    // enableRotation: true,
                     onPageChanged: (index) {
                       setState(() {
                         currentIndex = index;
                       });
                     },
-                    itemCount: widget.images.length,
+                    itemCount: images.length,
+                    gaplessPlayback: false,
+                    loadingBuilder: (context, progress) => Center(
+                          child: Container(
+                            width: 20.0,
+                            height: 20.0,
+                            child: CircularProgressIndicator(
+                                value: progress == null
+                                    ? null
+                                    : progress.cumulativeBytesLoaded /
+                                        progress.expectedTotalBytes),
+                          ),
+                        ),
                     builder: (BuildContext context, int index) {
                       return PhotoViewGalleryPageOptions(
-                          imageProvider: NetworkImage(widget.images[index]));
+                          imageProvider: NetworkImage(images[index]));
                     }),
+              )),
+          Positioned(
+              top: MediaQuery.of(context).padding.top + 15,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Text("${currentIndex + 1}/${images.length}",
+                    style: TextStyle(color: Colors.white, fontSize: 16)),
               )),
           Positioned(
               right: 10,
