1ef0c360eaa5e438ea7cacb843fc0af365e0fcce
cocosongying <cocosongying@gmail.com>
2020-09-23 23:21:40 +0800

    feat:novel menu

--- lib/views/novel/novel_reader_page.dart
+++ lib/views/novel/novel_reader_page.dart
@@ -10,6 +10,19 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
   List<Chapter> chapters = [];
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   var _lastPressed;
+  static final double _addBookshelfWidth = 95;
+  static final double _bottomHeight = 200;
+  static final double _sImagePadding = 20;
+  double _height = 0;
+  double _bottomPadding = _bottomHeight;
+  double _imagePadding = _sImagePadding;
+  double _addBookshelfPadding = _addBookshelfWidth;
+  int _duration = 200;
+  double _spaceValue = 1.8;
+  double _textSizeValue = 18;
+  bool _isNighttime = false;
+  bool _isAddBookshelf = false;
+  ScrollController _controller;
 
   @override
   void initState() {
@@ -123,7 +136,10 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
 
   void closeSetting() {
     setState(() {
-      //
+      _bottomPadding = _bottomHeight;
+      _height = 0;
+      _imagePadding = _sImagePadding;
+      _addBookshelfPadding = _addBookshelfWidth;
     });
   }
 
@@ -136,31 +152,163 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
           height: MediaQuery.of(context).padding.top,
         ),
         AnimatedContainer(
-          height: 15,
-          duration: Duration(milliseconds: 3000),
+          height: _height,
+          duration: Duration(milliseconds: _duration),
           child: Container(
-            height: 30,
+            height: 48,
             child: Row(
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Container(),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: Icon(Icons.arrow_back),
+                    ),
+                  ),
+                ),
+                Expanded(child: SizedBox()),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      //
+                    },
+                    child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Icon(Icons.share)),
+                  ),
+                ),
               ],
             ),
           ),
         ),
         Expanded(child: SizedBox()),
+        // Container(
+        //   margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+        //   width: _sImagePadding * 2,
+        //   height: _sImagePadding * 2,
+        //   child: AnimatedPadding(
+        //     duration: Duration(milliseconds: _duration),
+        //     padding: EdgeInsets.fromLTRB(
+        //         _imagePadding, _imagePadding, _imagePadding, _imagePadding),
+        //     child: InkWell(
+        //       onTap: () {
+        //         setState(() {
+        //           _isNighttime = !_isNighttime;
+        //         });
+        //       },
+        //       child: Icon(
+        //         _isNighttime ? Icons.access_alarm : Icons.monochrome_photos,
+        //       ),
+        //     ),
+        //   ),
+        // ),
         SizedBox(
           height: 20,
         ),
-        Container(
-          height: 15,
-          child: AnimatedPadding(
-            duration: Duration(milliseconds: 3000),
-            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        AnimatedContainer(
+          height: _bottomHeight,
+          duration: Duration(milliseconds: _duration),
+          // child: AnimatedPadding(
+            // duration: Duration(milliseconds: _duration),
+            padding: EdgeInsets.fromLTRB(0, _bottomPadding, 0, 0),
             child: Container(
-              child: Column(
+              height: _bottomHeight,
+              padding: EdgeInsets.fromLTRB(15, 20, 15, 15),
+              child: SingleChildScrollView(
+                child: Column(
                 children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "字号",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Icon(Icons.title),
+                      Expanded(
+                        child: SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTickMarkColor: Colors.transparent,
+                            inactiveTickMarkColor: Colors.transparent,
+                            trackHeight: 2.5,
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 8),
+                          ),
+                          child: Slider(
+                            value: _textSizeValue,
+                            label: "字号：$_textSizeValue",
+                            divisions: 20,
+                            min: 10,
+                            max: 30,
+                            onChanged: (double value) {
+                              setState(() {
+                                _textSizeValue = value;
+                              });
+                            },
+                            onChangeEnd: (value) {
+                              // _spSetTextSizeValue(value);
+                            },
+                          ),
+                        ),
+                      ),
+                      Icon(Icons.title),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "间距",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(
+                        width: 14,
+                      ),
+                      Icon(Icons.format_size),
+                      Expanded(
+                        child: SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTickMarkColor: Colors.transparent,
+                            inactiveTickMarkColor: Colors.transparent,
+                            trackHeight: 2.5,
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 8),
+                          ),
+                          child: Slider(
+                            value: _spaceValue,
+                            label: "字间距：$_spaceValue",
+                            min: 1.0,
+                            divisions: 20,
+                            max: 3.0,
+                            onChanged: (double value) {
+                              setState(() {
+                                _spaceValue = value;
+                              });
+                            },
+                            onChangeEnd: (value) {
+                              // _spSetSpaceValue(value);
+                            },
+                          ),
+                        ),
+                      ),
+                      Icon(Icons.format_size),
+                    ],
+                  ),
                   SizedBox(
                     height: 32,
                   ),
@@ -179,9 +327,10 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
                   ),
                 ],
               ),
+              ),
             ),
           ),
-        ),
+        // ),
       ],
     );
   }
@@ -196,9 +345,74 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
           children: <Widget>[
             GestureDetector(
               onTap: () {
-                //
-                _scaffoldKey.currentState.openDrawer();
+                setState(() {
+                  _bottomPadding == 0
+                      ? _bottomPadding = _bottomHeight
+                      : _bottomPadding = 0;
+                  _height == 48 ? _height = 0 : _height = 48;
+                  _imagePadding == 0
+                      ? _imagePadding = _sImagePadding
+                      : _imagePadding = 0;
+                  _addBookshelfPadding == 0
+                      ? _addBookshelfPadding = _addBookshelfWidth
+                      : _addBookshelfPadding = 0;
+                });
+                // _scaffoldKey.currentState.openDrawer();
               },
+              child: SingleChildScrollView(
+                controller: _controller,
+                padding: EdgeInsets.fromLTRB(
+                  16,
+                  16 + MediaQuery.of(context).padding.top,
+                  9,
+                  0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text('title'),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text('content------------'),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        MaterialButton(
+                          minWidth: 125,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(125)),
+                            side: BorderSide(width: 1),
+                          ),
+                          onPressed: () {
+                            //
+                          },
+                          child: Text("上一章"),
+                        ),
+                        MaterialButton(
+                          minWidth: 125,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(125)),
+                            side: BorderSide(width: 1),
+                          ),
+                          onPressed: () {
+                            //
+                          },
+                          child: Text("下一章"),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                  ],
+                ),
+              ),
             ),
             buildSetting(),
           ],
@@ -213,20 +427,6 @@ class _NovelReaderPageState extends State<NovelReaderPage> with RouteAware {
           return true;
         },
       ),
-      // body: AnnotatedRegion(
-      //   value: SystemUiOverlayStyle.dark,
-      //   child: Stack(
-      //     children: <Widget>[
-      //       Positioned(
-      //         left: 0,
-      //         top: 0,
-      //         right: 0,
-      //         bottom: 0,
-      //         child: Image.asset(Constant.ASSETS_IMG + "read_bg.png"),
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
